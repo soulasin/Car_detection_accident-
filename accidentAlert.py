4fe2cf3ded7fee74683de175bc48b2f460e572bb
0XS1N <134759972+soulasin@users.noreply.github.com>
2025-03-11 19:49:04 +0700

    Update accidentAlert.py

--- accidentAlert.py
+++ accidentAlert.py
@@ -15,7 +15,7 @@ frame_count = 0
 
 # Telegram Bot settings
 TELEGRAM_BOT_TOKEN = ""  # Token 
-TELEGRAM_CHAT_ID = "l"  # Chat ID  
+TELEGRAM_CHAT_ID = ""  # Chat ID  
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"  # sendMessage
     payload = {
